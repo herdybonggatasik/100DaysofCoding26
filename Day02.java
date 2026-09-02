6296f51f9b73f015a20778eed2c99c72b00f8dbd
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-03 07:29:52 +0800

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,18 @@
+public class Biodata {
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Program Biodata");
+        System.out.println("===============");
+        System.out.println("Nama: Herdi Bonggatasik");
+        System.out.println("Alamat: Mamasa, Sulawesi Barat");
+        System.out.println("Prodi: Informatika");
+        System.out.println("Program Selesai");
+        System.out.println("===============");
+        
+        
+}
+
+	
+}
