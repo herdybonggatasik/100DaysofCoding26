76d663f078c119285ee46487b68989dad6b7edbe
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-02 21:30:26 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+    // membuat variabel
+    String nama, alamat;
+    int usia;
+    double tinggi;
+    
+    // mengisi variabel
+    nama = "Petani Kode";
+    alamat = "Internet";
+    usia = 20;
+    tinggi = 164,6;
+    
+    //mencetak ke layar isi variabel
+    System.out.println("Herdi" + nama);
+    System.out.println("Mamasa" + alamat);
+    System.out.println("20" +usia + "2026");
+    System.out.println("164,6" + tinggi +"20");
+}
