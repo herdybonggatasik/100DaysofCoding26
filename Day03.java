73b469d165a2b76337a61e2bd9d364747799fc0f
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-04 07:17:48 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,11 @@
+public class main  {
+    public static void main(String[] args) {
+        // Contoh Penggunaan Kombinasi Escape Sequence
+        System.out.println("DAFTAR SISWA:\n");
+        System.out.println("1.\tNama:\t\"Herdi Bonggatasik\"\n\tUmur:\t18");
+        System.out.println("2.\tNama:\t\"Dirly\"\n\tUmur:\t19");
+        
+        // Contoh Path File
+        System.out.println("\nLokasi File: C:\\Users\\Public\\Document.txt");
+    }
+}
