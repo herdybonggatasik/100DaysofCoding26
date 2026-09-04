a9a5d6398498f3157d2ecc93287b61cab1088284
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-04 12:21:53 +0800

    Update Day03.java

--- Day03.java
+++ Day03.java
@@ -4,6 +4,22 @@ public class main  {
         System.out.println("DAFTAR SISWA:\n");
         System.out.println("1.\tNama:\t\"Herdi Bonggatasik\"\n\tUmur:\t18");
         System.out.println("2.\tNama:\t\"Dirly\"\n\tUmur:\t19");
+
+        // \b (Backspace)
+        System.out.println("Halo\b Dunia");
+
+        // \r (Carriage Return)
+        System.out.println("Teks Awal\rMenimpa");
+
+        // \' (Single Quote)
+        System.out.println("Karakter petik tunggal: \'A\'");
+
+        // \" (Double Quote)
+        System.out.println("Dia berkata, \"Selamat Pagi!\"");
+
+        // \\ (Backslash)
+        System.out.println("Folder: C:\\Program Files\\Java")
+
         
         // Contoh Path File
         System.out.println("\nLokasi File: C:\\Users\\Public\\Document.txt");
