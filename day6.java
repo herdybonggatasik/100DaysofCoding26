b3b7b05759512be5c36a0af5722535b2df29fbf4
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-07 13:32:31 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tipe data karakter (char)
+        char jenisKelamin = 'L';
+        char simbol = '$';
+        
+        // Tipe data boolean (boolean)
+        boolean lulusTidak = true;
+        boolean hujanTidak = false;
+        
+        // Tampilan Hasil
+        System.out.println("--- Tipe Data Char ---");
+        System.out.println("jenisKelamin = " +jenisKelamin);
+        System.out.println("simbol = " +simbol);
+        
+        System.out.println("--- Tipe Data Boolean---");
+        System.out.println("lulus tidak = " +lulusTidak);
+        System.out.println("hujan tidak = " +hujanTidak);
+        
+    }
+}
