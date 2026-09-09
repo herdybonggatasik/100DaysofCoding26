61bc8de96191d704eb4fb7eac2de87ad5b0212d4
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-09 08:50:49 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args){
+		
+		// Membuat Variabel Tanpa Isi (Deklarasi Variabel)
+		    int umur = 18;
+		
+		// Memberikan Nilai Pertama (Inisialisasi Variabel)
+		    umur = 18;
+		    System.out.println("Umur Awal: " + umur);
+		
+		// Deklarasi Dan Inisialisasi Dalam Satu Baris
+		    String nama = "Herdi Bonggatasik";
+		    System.out.println("Nama: " + nama);
+		
+		// Mengubah Nilai Yang Sudah Ada (Update Variabel)
+		    umur = 20;
+		    System.out.println("Umur Setelah Update: " + umur);
+	}
+}
