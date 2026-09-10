33efac135fd0925f376f3e3c58941cd25aeb203f
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-10 11:27:25 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		// MENGGUNAKAN double UNTUK DATA DENGAN DESIMAL (KOMA)
+		final double TINGGI_BADAN =  164.5;
+		
+		// MENGGUNAKAN int UNTUK DATA BILANGAN BULAT
+		final int UMUR = 18;
+		final int TAHUN_LAHIR = 2008;
+		
+		// MENAMPILKAN HASIL/DATA 
+		System.out.println("Tinggi Badan: " + TINGGI_BADAN + " cm");
+		System.out.println("Umur: " + UMUR + " tahun");
+		System.out.println("Tahun Lahir: " + TAHUN_LAHIR);
+		
+	}
+}
