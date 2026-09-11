f8d5ab657c9142448ff156a1a1dbc13b6b6fb86e
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-11 17:34:28 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,46 @@
+public class Main {
+   public static void main(String[] args) {
+		// Tipe data byte
+		System.out.println("==== BYTE ====");
+		System.out.println("Min: " + Byte.MIN_VALUE);
+		System.out.println("Max: " + Byte.MAX_VALUE);
+		
+   	
+		// Tipe data short
+		System.out.println("==== SHORT ====");
+		System.out.println("Min: " + Short.MIN_VALUE);
+		System.out.println("Max: " + Short.MAX_VALUE);
+		
+   	
+		// Tipe data integer
+		System.out.println("==== INTEGER ====");
+		System.out.println("Min: " + Integer.MIN_VALUE);
+		System.out.println("Max: " + Integer.MAX_VALUE);
+		
+   	
+		// Tipe data long
+		System.out.println("==== LONG ====");
+		System.out.println("Min: " + Long.MIN_VALUE);
+		System.out.println("Max: " + Long.MAX_VALUE);
+		
+   	
+		// Tipe data float
+		System.out.println("==== FLOAT ====");
+		System.out.println("Min (Positif Terkecil): " + Float.MIN_VALUE);
+		System.out.println("Max: " + Float.MAX_VALUE);
+		
+   	
+		// Tipe data double
+		System.out.println("==== DOUBLE ====");
+		System.out.println("Min (Positif Terkecil): " + Double.MIN_VALUE);
+		System.out.println("Max: " + Double.MAX_VALUE);
+   	
+   	
+    // Tipe data char	
+   	System.out.println("==== CHARACTER ====");
+    System.out.println("Min: " + Character.MIN_VALUE);
+    System.out.println("Max: " + Character.MAX_VALUE);
+   	
+   }
+	
+}
