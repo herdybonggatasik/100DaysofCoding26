36c7bd7e51a047e0c49b9ac64eae61421e69d6fb
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-15 18:33:49 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+        // Deklarasi variabel angka
+        int angka1 = 5;
+        int angka2 = 4;
+        
+        // Operasi penjumlahan (+)
+        int hasilPenjumlahan = (angka1+angka2);
+        
+        // Operasi pengurangan (-)
+        int hasilPengurangan = (angka1-angka2);
+        
+        // Menampilkan output/hasil
+        System.out.println("Angka 1: " + angka1);
+        System.out.println("Angka 2: " + angka2);
+        
+        System.out.println("Hasil Penjumlahan (+): " + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan (-): " + hasilPengurangan);
+        
+    }
+}
