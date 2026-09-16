9b05dcb7d5650b1962062a0548a117c33a67ac88
herdybonggatasik <hherdibonggatasik@gmail.com>
2026-09-16 18:24:56 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,22 @@
+public class Pp{
+    public static void main(String[] args){
+    
+    // Membuat Variabel
+    int angka1Her = 10;
+    int angka2Her = 5;
+    
+    // Hasil Perkalian (*)
+    int hasilPerkalian = (angka1Her*angka2Her);
+    
+    // Hasil Pembagi (/)
+    int hasilPembagi = (angka1Her/angka2Her);
+        
+        System.out.println("Angka 1 Her: " + angka1Her);
+        System.out.println("Angka 2 Her: " + angka2Her);
+    
+    System.out.println("Hasil Perkalian (*): " + hasilPerkalian);
+        System.out.println("Hasil Pembagi (/) " + hasilPembagi);
+    
+    }
+    
+}
